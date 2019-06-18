286418a087e4f06e5249e6ff398a07329e657bf8
Krzysztof Koziol <krzysiek.koziol@gmail.com>
2019-06-18 14:23:10 +0200

    Fixed test configuration

--- src/test/kotlin/com/dietmap/yaak/SupportController.kt
+++ src/test/kotlin/com/dietmap/yaak/SupportController.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
@@ -12,6 +13,7 @@ import org.springframework.web.context.WebApplicationContext
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@TestPropertySource(properties = ["YAAK_API_KEY = MyApiKey123"])
 abstract class SupportController {
 
     @Autowired
