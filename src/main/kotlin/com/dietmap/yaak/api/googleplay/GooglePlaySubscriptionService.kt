ad39564f7cdf2c327794269fcc02d63b5024b9ae
Marek Bryla <patrafter2@gmail.com>
2019-11-26 16:35:58 +0100

    Add Google PubSub subscriptions notification handler (purchase only)

--- src/main/kotlin/com/dietmap/yaak/api/googleplay/GooglePlaySubscriptionService.kt
+++ src/main/kotlin/com/dietmap/yaak/api/googleplay/GooglePlaySubscriptionService.kt
@@ -1,5 +1,6 @@
 package com.dietmap.yaak.api.googleplay
 
+import com.dietmap.yaak.api.googleplay.GooglePlayNotificationType.*
 import com.dietmap.yaak.domain.userapp.NotificationType
 import com.dietmap.yaak.domain.userapp.UserAppClient
 import com.dietmap.yaak.domain.userapp.UserAppSubscriptionNotification
@@ -25,10 +26,12 @@ class GooglePlaySubscriptionService(val androidPublisherApiClient: AndroidPublis
         private val logger: Logger = LoggerFactory.getLogger(GooglePlaySubscriptionService::class.java)
     }
 
-    fun handlePurchase(packageName: String, subscriptionId: String, purchaseToken: String, userEmail: String): SubscriptionPurchase? {
+    fun handlePurchase(packageName: String, subscriptionId: String, purchaseToken: String, userEmail: String?): SubscriptionPurchase? {
         val subscriptionPurchase = androidPublisherApiClient.Purchases().Subscriptions().get(packageName, subscriptionId, purchaseToken).execute()
         //TODO translate into domain exceptions
-        Preconditions.checkArgument(subscriptionPurchase.emailAddress == userEmail, "Purchased subscription email does not match given email=$userEmail")
+        userEmail.apply {
+            Preconditions.checkArgument(subscriptionPurchase.emailAddress == this, "Purchased subscription email does not match given email=$userEmail")
+        }
         Preconditions.checkState(subscriptionPurchase.paymentState in listOf(PAYMENT_RECEIVED_CODE, PAYMENT_FREE_TRIAL_CODE), "Subscription has not been paid yet, paymentState=${subscriptionPurchase.paymentState}")
         try {
             val notificationResponse = userAppClient.sendSubscriptionNotification(UserAppSubscriptionNotification(
@@ -51,6 +54,26 @@ class GooglePlaySubscriptionService(val androidPublisherApiClient: AndroidPublis
         } catch (e: WebClientResponseException) {
             //TODO make client return domain exceptions
             throw ResponseStatusException(e.statusCode, "UserAppSubscriptionNotification error", e)
+        }
+    }
+
+    fun handleSubscriptionNotification(pubsubNotification: PubSubDeveloperNotification) {
+        pubsubNotification.subscriptionNotification?.let {
+            when(it.notificationType) {
+                SUBSCRIPTION_PURCHASED -> handlePurchase(pubsubNotification.packageName, it.subscriptionId, it.purchaseToken, null)
+                SUBSCRIPTION_RECOVERED -> TODO()
+                SUBSCRIPTION_RENEWED -> TODO()
+                SUBSCRIPTION_CANCELED -> TODO()
+                SUBSCRIPTION_ON_HOLD -> TODO()
+                SUBSCRIPTION_IN_GRACE_PERIOD -> TODO()
+                SUBSCRIPTION_RESTARTED -> TODO()
+                SUBSCRIPTION_PRICE_CHANGE_CONFIRMED -> TODO()
+                SUBSCRIPTION_DEFERRED -> TODO()
+                SUBSCRIPTION_PAUSED -> TODO()
+                SUBSCRIPTION_PAUSE_SCHEDULE_CHANGED -> TODO()
+                SUBSCRIPTION_REVOKED -> TODO()
+                SUBSCRIPTION_EXPIRED -> TODO()
+            }
         }
     }
 }
